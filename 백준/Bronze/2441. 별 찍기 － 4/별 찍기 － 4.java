10da62bf77a6ab93db3fa78c279d0199f92d8879
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2024-09-02 20:12:54 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			
+			int N = Integer.parseInt(br.readLine());
+			
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(j >= i) {
+						bw.write("*");
+					} else {
+						bw.write(" ");
+					}
+				}
+				bw.newLine();
+			}
+
+			bw.flush();
+			bw.close();
+		} catch (Exception e) {
+			
+		}
+		
+	}
+
+}
