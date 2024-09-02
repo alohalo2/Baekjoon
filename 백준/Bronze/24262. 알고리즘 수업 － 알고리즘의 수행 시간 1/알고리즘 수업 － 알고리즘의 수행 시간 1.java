63b155ae4c6fbd135329d0dd1f9788f6f4be6753
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2024-09-02 11:17:28 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			
+			bw.write(1 + "\n");
+			bw.write(0 + "\n");
+			
+			bw.flush();
+			bw.close();
+		} catch (Exception e) {
+			
+		}
+		
+	}
+
+}
