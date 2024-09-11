9516e47288436b27982af4b74f2eaa854b1ffe1d
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2024-09-11 20:32:01 +0900

    [level 1] Title: 소수 만들기, Time: 10.71 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -3,9 +3,9 @@ class Solution {
         int answer = 0;
         int sum = 0;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
+        for (int i = 0; i < nums.length - 2; i++) { // 배열에서 인접하지 않은 3개의 수를 더하기 위해 for문 3개 사용
+            for (int j = i + 1; j < nums.length - 1; j++) { // 대신 i = 0, j = i + 1, k = j + 1 이런 식으로 중복 
+                for (int k = j + 1; k < nums.length; k++) { // index를 제거함
                     sum = nums[i] + nums[j] + nums[k];
                     
                     if(isPrime(sum)) {
