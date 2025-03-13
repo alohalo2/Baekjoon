c2bfa65ea6f223a2ed97d2ae1df6d33adaef2b5c
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2025-03-13 16:59:34 +0900

    [Bronze III] Title: ACM 호텔, Time: 132 ms, Memory: 16256 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			
+			int testCount = Integer.parseInt(br.readLine());
+			
+			while(true) {
+				if(testCount == 0) break;
+
+				String[] userInput = br.readLine().split(" ");
+				int numberOfFloor = Integer.parseInt(userInput[0]);
+				int roomQuantity = Integer.parseInt(userInput[1]);
+				int numberOfCustomer = Integer.parseInt(userInput[2]);
+				
+				System.out.println(calcRoomNumber(numberOfFloor, roomQuantity, numberOfCustomer));
+				
+				testCount--;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static int calcRoomNumber(int numberOfFloor, int roomQuantity, int numberOfCustomer) {
+		
+		
+		int customerFloor; 
+		int customerRoomNumber = (numberOfCustomer - 1) / numberOfFloor + 1;
+				
+		if(numberOfCustomer % numberOfFloor == 0) {
+			customerFloor = numberOfFloor;
+		} else {
+			customerFloor = numberOfCustomer % numberOfFloor;
+		}
+		
+		int result;
+		
+		if (customerRoomNumber < 10) {
+			result = Integer.parseInt(customerFloor + "0" + customerRoomNumber);
+		} else {
+			result = Integer.parseInt(customerFloor + "" + customerRoomNumber);
+		}
+		
+		return result;
+	}
+
+}
