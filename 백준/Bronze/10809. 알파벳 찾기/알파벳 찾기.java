b738fc86fa8b10596ec9d9b321e9f21689918c51
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2025-03-20 16:31:44 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 120 ms, Memory: 15948 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+			
+			String userInput = br.readLine().toLowerCase();
+			
+			int[] resultArry = new int[26];
+
+			Arrays.fill(resultArry, -1);
+			
+			for(int i = 0; i < userInput.length(); i++) {
+				char ch = userInput.charAt(i);
+				int index = ch - 'a';
+				
+				if(resultArry[index] == -1) {
+					resultArry[index] = i;
+				}
+			}
+			
+			for (int i : resultArry) {
+				System.out.print(i + " ");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
