1c7080483c37363088eb82648189f08da24ab39a
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2025-03-27 15:39:25 +0900

    [Bronze I] Title: 팰린드롬수, Time: 100 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+			
+			while(true) {
+				String userInput = br.readLine();
+				
+				if(userInput.equals("0")) break;
+				
+				System.out.println(check(userInput));
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static String check(String userInput) {
+	    int len = userInput.length();
+	    
+	    for (int i = 0; i < len / 2; i++) {
+	        if (userInput.charAt(i) != userInput.charAt(len - 1 - i)) {
+	            return "no";
+	        }
+	    }
+	    return "yes";
+	}
+}
