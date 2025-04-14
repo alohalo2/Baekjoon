b6f9ab936e5ade80109dae965b1019ec5f670bea
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2025-04-14 16:08:58 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1768 ms, Memory: 188764 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+			
+			int numberQauntity = Integer.parseInt(br.readLine());
+			
+			// HashSet은 중복만 없지만 TreeSet은 중복 + 정렬까지 된다.
+			Set<Integer> sortedSet = new TreeSet<>();
+			// system.out.println은 출력 버퍼 I/O 비용이 커서 시간초과가 난다.
+			StringBuilder sb = new StringBuilder();
+			
+			for (int i = 0; i < numberQauntity; i++) {
+				sortedSet.add(Integer.parseInt(br.readLine()));
+			}
+			
+			for (Integer i : sortedSet) {
+				sb.append(i).append("\n");
+			}
+			
+			System.out.print(sb);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
