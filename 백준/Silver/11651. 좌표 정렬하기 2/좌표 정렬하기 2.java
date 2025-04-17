f8274c18222715d348e9a434a7be109e8ec8367f
alohalo2 <161301242+alohalo2@users.noreply.github.com>
2025-04-17 16:21:25 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 676 ms, Memory: 52228 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+
+			int N = Integer.parseInt(br.readLine());
+			List<int[]> intArryList = new ArrayList<>();
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+				int[] intArry = new int[2];
+				intArry[0] = Integer.parseInt(st.nextToken());
+				intArry[1] = Integer.parseInt(st.nextToken());
+				intArryList.add(intArry);
+			}
+
+			intArryList.sort(
+					Comparator.comparingInt((int[] num) -> num[1])
+							.thenComparingInt(num -> num[0]));
+
+			for (int i = 0; i < N; i++) {
+				sb.append(intArryList.get(i)[0]).append(" ").append(intArryList.get(i)[1]).append("\n");
+			}
+
+			System.out.println(sb);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
